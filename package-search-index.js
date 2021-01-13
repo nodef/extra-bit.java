6022a4a3ca8bddacee81b09042d86db2e7a5ea96
Subhajit Sahu <wolfram77@gmail.com>
2021-01-13 22:53:17 +0530

    Publishing javadoc for extra-bit:0.0.11

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.javaf"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.javaf"}]
--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"io.github.javaf","l":"Bit"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"io.github.javaf","l":"Bit"}]
